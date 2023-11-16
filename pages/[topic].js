03434baf64b08d2f055bdccc0f275ce0a073e897
tt <sydney118227@icloud.com>
2023-11-16 11:06:56 +0800

    3.0

--- pages/[topic].js
+++ pages/[topic].js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const pathname = usePathname();
-  if (pathname === "studyabroad") {
+  if (pathname === "/studyabroad") {
     return (
       <div className="bg-black flex-1 flex min-h-screen h-full w-full justify-center items-center">
         <iframe
