7a06ff2290b096b6c215993bea2174d583b54bc1
tt <sydney118227@icloud.com>
2023-12-27 16:45:34 +0800

    3.0

--- pages/[topic].js
+++ pages/[topic].js
@@ -7,32 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const pathname = usePathname();
-  if (pathname === "/studyabroad") {
-    return (
-      <div className="bg-black flex-1 flex min-h-screen h-full w-full justify-center items-center">
-        <iframe
-          src="https://m-studyabroad-3xm.vercel.app/studyabroad"
-          frameborder="0"
-          style={{
-            height: "100%",
-            width: "100%",
-            top: "0px",
-            left: "0px",
-            right: "0px",
-            bottom: "0px",
-            position: "absolute",
-          }}
-        ></iframe>
-      </div>
-    );
-  } else {
-    return (
-      <div className="bg-black flex-1 flex min-h-screen h-full w-full justify-center items-center">
-        <HomePage topic={pathname?.split("/")[1]} />
 
-        {/* <h1 className="text-[200px] text-red-500">{pathname?.split("/")[1]}</h1>
-      <h1 className="text-[200px] text-red-500">{searchParams}</h1> */}
-      </div>
-    );
-  }
+  return (
+    <div className="bg-black flex-1 flex min-h-screen h-full w-full justify-center items-center">
+      <HomePage topic={pathname?.split("/")[1]} />
+    </div>
+  );
 }
