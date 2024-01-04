25fddb52b6bfa5874a4268bd23d4e2b3ffd20200
tt <sydney118227@icloud.com>
2024-01-04 14:54:44 +0800

    3.0

--- pages/api/db.js
+++ pages/api/db.js
@@ -90,7 +90,7 @@ export default async function handler(req, res) {
               FirstName: name,
               Country: country,
               Topic: topic,
-              date: moment().format("YYYY-MM-DD"),
+              Date: moment().format("YYYY-MM-DD"),
             },
           }),
         }
