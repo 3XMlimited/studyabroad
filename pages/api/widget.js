25b72df05aa4f1ea3bb7cbaa017d07e2143537c5
tt <sydney118227@icloud.com>
2024-06-20 09:56:58 +0800

    5.8

--- pages/api/widget.js
+++ pages/api/widget.js
@@ -1,31 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const connectDB = async (db_name, collection_name, condition) => {
-  // const client = await MongoClient.connect(process.env.MONGODB_URL, {
-  //   // useNewUrlParser: true,
-  // }).catch((err) => {
-  //   console.log(err);
-  // });
-
-  // if (!client) {
-  //   return;
-  // }
-  // try {
-  //   const db = client.db(db_name);
-
-  //   let collection = db.collection(collection_name);
-  //   let checkResult = await collection.findOne(condition);
-  //   if (checkResult) {
-  //     return checkResult;
-  //   } else {
-  //     return [];
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   return [];
-  // } finally {
-  //   client.close();
-  // }
+const connectDB = async (db_name, collection_name, condition, country) => {
   const client = new MongoClient(process.env.MONGODB_URL, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -42,7 +17,8 @@ const connectDB = async (db_name, collection_name, condition) => {
     let result = await collection.findOne(condition);
     // console.log(result);
     if (result) {
-      return result;
+      const data = await randomUrl(result, country);
+      return data;
     } else {
       return [];
     }
@@ -54,12 +30,71 @@ const connectDB = async (db_name, collection_name, condition) => {
   }
 };
 
-export const countView = async (
-  db_name,
-  collection_name,
-  condition,
-  update
-) => {
+const randomUrl = async (result, country) => {
+  let url = "";
+  if (result.case.includes("All")) {
+    let uri = result.url?.action;
+    let urls = [];
+
+    Object.values(uri).map((r, i) => {
+      if (r.state === true) {
+        urls.push({
+          client: Object.keys(uri)[i],
+          value: r["%"] != 0 ? r["%"] / 100 : 0,
+        });
+      }
+    });
+
+    var randomNumber = Math.random();
+    var randomKey;
+    let value = 0;
+    for (let i = 0; i < urls.length; i++) {
+      const element = urls[i];
+      value += element.value;
+      if (randomNumber < value) {
+        randomKey = element.client;
+        break;
+      } else if (i === urls.length) {
+        randomKey = element.client;
+      }
+    }
+    url = result.url?.url[randomKey];
+    if (result.url.count) {
+      if (result.url.count[`${randomKey}`]) {
+        result.url.count[`${randomKey}`] += 1;
+      } else {
+        result.url.count[`${[randomKey]}`] = 1;
+      }
+    } else {
+      result.url.count = {};
+      result.url.count[`${[randomKey]}`] = 1;
+    }
+  } else if (result.case.includes("Country")) {
+    try {
+      url = result.url.url[`${country}`];
+      if (url === undefined) {
+        url = result.url.url[`OTHER`];
+      }
+
+      if (result.url.count) {
+        if (result.url.count[`${country}`]) {
+          result.url.count[`${country}`] += 1;
+        } else {
+          result.url.count[`${[country]}`] = 1;
+        }
+      } else {
+        result.url.count = {};
+        result.url.count[`${[country]}`] = 1;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  await countView("Market_V2", "indexes", result);
+  return url;
+};
+
+export const countView = async (db_name, collection_name, update) => {
   const client = new MongoClient(process.env.MONGODB_URL, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -74,9 +109,12 @@ export const countView = async (
     const db = client.db(db_name);
     let collection = db.collection(collection_name);
 
-    let result = await collection.updateOne(condition, {
-      $set: { url: update },
-    });
+    let result = await collection.updateOne(
+      { widget: update.widget },
+      {
+        $set: { url: update.url },
+      }
+    );
 
     if (result) {
       return result;
@@ -98,8 +136,11 @@ export default async function handler(req, res) {
       const body = await req.body;
       console.log(body);
       const widget = Number(body?.widget);
+      const country = body.country;
+      console.log(country);
+      let data = await connectDB("Market_V2", "indexes", { widget }, country);
 
-      let data = await connectDB("Market_V2", "indexes", { widget });
+      // const result = await randomUrl(data);
 
       return res.json(data);
     } catch (error) {
@@ -115,9 +156,8 @@ export const PATCH = async (req) => {
     const body = await req.json();
 
     const widget = Number(body.widget);
-    const update = body.update;
 
-    const data = await countView("Market_V2", "indexes", { widget }, update);
+    const data = await countView("Market_V2", "indexes", { widget });
 
     // let data = await connectDB("Market_V2", "indexes", { widget });
 
--- pages/widget/[widget].js
+++ pages/widget/[widget].js
@@ -14,103 +14,115 @@ const page = ({ country }) => {
   const [isLoading, setIsLoading] = useState(false);
   country = decodeURIComponent(country);
 
-  const countView = async (update) => {
-    const response = await fetch("/api/widget", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        widget: pathname.split("/")[2],
-        update,
-      }),
-    });
+  // const countView = async (update) => {
+  //   try {
+  //     const response = await fetch("/api/widget", {
+  //       method: "PATCH",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         widget: pathname.split("/")[2],
+  //         update,
+  //       }),
+  //     });
 
-    const result = await response.json();
-    return;
-  };
+  //     const result = await response.json();
+  //     return;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   const fetchData = async () => {
     // console.log("pathname", pathname?.split("/")[2]);
-    setIsLoading(true);
-    let url = "";
-    if (pathname) {
-      const response = await fetch("/api/widget", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          widget: pathname?.split("/")[2],
-        }),
-      });
+    try {
+      setIsLoading(true);
+      let url = "";
+      if (pathname) {
+        const response = await fetch("/api/widget", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            widget: pathname?.split("/")[2],
+            country,
+          }),
+        });
 
-      const result = await response.json();
+        const result = await response.json();
 
-      if (result) {
-        // console.log(result);
-        //   let urlList = result.find(
-        //     (d) => d.widget.toString() === pathname.split("/")[1]
-        //   );
+        if (result) {
+          // console.log("1", result);
+          // console.log(result);
+          //   let urlList = result.find(
+          //     (d) => d.widget.toString() === pathname.split("/")[1]
+          //   );
 
-        if (result.case.includes("All")) {
-          let uri = result.url?.action;
-          let urls = [];
+          // if (result.case.includes("All")) {
+          //   let uri = result.url?.action;
+          //   let urls = [];
 
-          Object.values(uri).map((r, i) => {
-            if (r.state === true) {
-              urls.push({
-                client: Object.keys(uri)[i],
-                value: r["%"] != 0 ? r["%"] / 100 : 0,
-              });
-            }
-          });
+          //   Object.values(uri).map((r, i) => {
+          //     if (r.state === true) {
+          //       urls.push({
+          //         client: Object.keys(uri)[i],
+          //         value: r["%"] != 0 ? r["%"] / 100 : 0,
+          //       });
+          //     }
+          //   });
 
-          var randomNumber = Math.random();
-          var randomKey;
-          let value = 0;
-          for (let i = 0; i < urls.length; i++) {
-            const element = urls[i];
-            value += element.value;
-            if (randomNumber < value) {
-              randomKey = element.client;
-              break;
-            } else if (i === urls.length) {
-              randomKey = element.client;
-            }
+          //   var randomNumber = Math.random();
+          //   var randomKey;
+          //   let value = 0;
+          //   for (let i = 0; i < urls.length; i++) {
+          //     const element = urls[i];
+          //     value += element.value;
+          //     if (randomNumber < value) {
+          //       randomKey = element.client;
+          //       break;
+          //     } else if (i === urls.length) {
+          //       randomKey = element.client;
+          //     }
+          //   }
+          //   url = result.url?.url[randomKey];
+          //   //   if (result.url.count) {
+          //   //     if (result.url.count[`${randomKey}`]) {
+          //   //       result.url.count[`${randomKey}`] += 1;
+          //   //     } else {
+          //   //       result.url.count[`${[randomKey]}`] = 1;
+          //   //     }
+          //   //   } else {
+          //   //     result.url.count = {};
+          //   //     result.url.count[`${[randomKey]}`] = 1;
+          //   //   }
+          //   //   await countView(result.url);
+          // }
+
+          // else if (result.case.includes("Country")) {
+          //   try {
+          //     url = result.url.url[`${props.searchParams.country}`];
+          //     if (url === undefined) {
+          //       url = result.url.url[`OTHER`];
+          //     }
+          //   } catch (error) {
+          //     console.log(error);
+          //   }
+          // }
+
+          setData(result);
+          setIsLoading(false);
+          //   console.log(url);
+          if (url !== undefined) {
+            window.location.assign(result);
           }
-          url = result.url?.url[randomKey];
-          if (result.url.count) {
-            if (result.url.count[`${randomKey}`]) {
-              result.url.count[`${randomKey}`] += 1;
-            } else {
-              result.url.count[`${[randomKey]}`] = 1;
-            }
-          } else {
-            result.url.count = {};
-            result.url.count[`${[randomKey]}`] = 1;
-          }
-          await countView(result.url);
-        } else if (result.case.includes("Country")) {
-          try {
-            url = result.url.url[`${props.searchParams.country}`];
-            if (url === undefined) {
-              url = result.url.url[`OTHER`];
-            }
-          } catch (error) {
-            console.log(error);
-          }
+
+          return;
         }
-
-        setData(result);
-        setIsLoading(false);
-        //   console.log(url);
-        if (url !== undefined) {
-          window.location.assign(url);
-        }
-
-        return;
       }
+    } catch (error) {
+      console.log(error);
     }
   };
 
